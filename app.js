edb8d5f73d8e7ee68c71fd13e5e4200bec391b05
sreerag-pavithran <sreeragpavithran@gmail.com>
2022-05-29 11:05:21 +0530

    View Engine Setup

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ var adminRouter = require("./routes/admin");
 
 var app = express();
 
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
