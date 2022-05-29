559b57b07061d4bd073bbb2a98d792f30dd86364
sreerag-pavithran <sreeragpavithran@gmail.com>
2022-05-29 11:13:26 +0530

    View Engine Setup

--- app.js
+++ app.js
@@ -27,4 +27,15 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/admin", adminRouter);
 
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
 module.exports = app;
